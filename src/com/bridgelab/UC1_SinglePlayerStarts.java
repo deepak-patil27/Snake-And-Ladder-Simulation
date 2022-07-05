0e366cf7b303723f08b17a8f78013f5ca7fa205e
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-06 00:33:03 +0530

    Added: Single player At Start Position Zero

---
+++ src/com/bridgelab/UC1_SinglePlayerStarts.java
@@ -0,0 +1,12 @@
+package com.bridgelab;
+
+public class UC1_SinglePlayerStarts {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Snake And Ladder game.");
+
+	           int startPosition=0;
+	           
+	        System.out.println("you are at start position");
+	    }
+
+}
