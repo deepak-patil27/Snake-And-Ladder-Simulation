6455c8d41993e25bc3610b5363238e16fb9a2d0c
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-06 00:26:55 +0530

    Added: Welcome to Snake & Ladder Simulation

---
+++ src/com/bridgelab/SnakeAndLadderWlcmProg.java
@@ -0,0 +1,9 @@
+package com.bridgelab;
+
+public class SnakeAndLadderWlcmProg {
+	public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder game.");
+
+    }
+
+}
