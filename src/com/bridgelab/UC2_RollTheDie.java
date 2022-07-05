901feafe7b83215e5d4b84c762b4dfb5e6ee1b75
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-07-06 00:38:49 +0530

    Added: Roll The Die To Get Number

---
+++ src/com/bridgelab/UC2_RollTheDie.java
@@ -0,0 +1,12 @@
+package com.bridgelab;
+
+public class UC2_RollTheDie {
+	 public static void main(String[] args) {
+	        System.out.println("Welcome to Snake And Ladder game.");
+
+	           int startPosition=0;
+	           int DieRolled = (int) (Math.random()*6)+1;
+	           System.out.println(DieRolled);
+	    }
+
+}
